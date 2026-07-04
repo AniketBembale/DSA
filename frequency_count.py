fd316c240c6c6b5c1a1965dce7f84392d286da7a
Aniket Bembale <aniket.bembale@cloudthat.com>
2026-07-04 23:46:37 +0530

    DSA-Day-3

--- frequency_count.py
+++ frequency_count.py
@@ -1,21 +1,45 @@
-# def freq_Count(n):
-#     freq_hash = {}
+def freq_count(nums):
+    freq_map = {}
 
-#     for i in range(0,len(n)):
-#         if n[i] in freq_hash:
-#             freq_hash[n[i]] += 1
-#         else:
-#             freq_hash[n[i]]=1
-#     return freq_hash
-# print(freq_Count([1,1,2,2,4,5,6,7,7]))
+    for i in nums:
+        if i in freq_map:
+            freq_map[i]+=1
+        else:
+            freq_map[i]=1
+    return freq_map
+
+print(freq_count([1,1,2,4,5,5,6,6,6,6,7]))
 
 
 
-def freq_Count(list1):
-    freq_hash = {}
-    n = len(list1)
+def freq_count(nums):
+    freq_map = {}
+    for i in range(0,len(nums)):
+        if nums[i] in freq_map:
+            freq_map[nums[i]] += 1
+        else:
+            freq_map[nums[i]] = 1
+    return freq_map
+        
 
-    for i in range(0,n):
-        freq_hash[list1[i]] = freq_hash.get(list1[i],0)+1
-    return freq_hash
-print(freq_Count([1,1,2,2,4,5,6,7,7]))
+print(freq_count([1,1,2,4,5,5,6,6,6,6,7]))
+
+
+
+
+def freq_count(nums):
+    freq_map = {}
+    for i in nums:
+        freq_map[i] = freq_map.get(i,0)+1
+    return freq_map
+
+
+  
+        
+
+print(freq_count([1,1,2,4,5,5,6,6,6,6,7]))
+
+
+
+
+
