85ad049c00f25f6f571768fb521035c8561ca186
Aniket Bembale <aniket.bembale@cloudthat.com>
2026-07-06 00:14:03 +0530

    DSA-Day-5

--- hash.py
+++ hash.py
@@ -27,18 +27,40 @@ m = [1,3,4,5,6,13,23,44,43,10,61,71,11,22]
 #         print(f"{i}:{hash_list[i]}")
 
 
-freq_dict = {}
-for i in n:
-    if i in freq_dict:
-        freq_dict[i] +=1
-    else:
-        freq_dict[i] = 1
+# freq_dict = {}
+# for i in n:
+#     if i in freq_dict:
+#         freq_dict[i] +=1
+#     else:
+#         freq_dict[i] = 1
 
-for i in m:
-    if i<1 or i>10:
-        print(0)
-    else:
-        print((f"{i}:{freq_dict[i]}"))
+# for i in m:
+#     if i<1 or i>10:
+#         print(0)
+#     else:
+#         print((f"{i}:{freq_dict[i]}"))
 
 
-        
+
+
+
+word = "ssaavvbewwwwwwwwe"
+# for lower char asci number between 97 - 122
+char_list = ['a','b','c','s','v','e','w']
+
+
+
+# print(ord("a"))
+
+asci_list = [0]*26
+for i in word:
+    index = ord(i)-97
+    asci_list[index]+=1
+
+
+for i in char_list:
+    index = ord(i)-97
+    print(f"{i}:{asci_list[index]}")
+
+
+
